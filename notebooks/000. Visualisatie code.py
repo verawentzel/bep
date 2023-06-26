e73033e92756367c142462353993e0e0ed150066
verawentzel <133372435+verawentzel@users.noreply.github.com>
2023-06-26 20:32:45 +0200

    26.06 Had alles maar ga ik nu aanpassen naar alles met scaffold en EFP & mol descr. combined

--- notebooks/000. Visualisatie code.py
+++ notebooks/000. Visualisatie code.py
@@ -2,25 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
-colors = [ (0.290, 0.380, 0.619), (0.529, 0.808, 0.980)] # Lichtgroen naar middelgroen
-#colors = [(0.0, 0.5, 0.0), (0.85, 1.0, 0.85)]
+#colors = [(0.773, 0.890, 0.933),  (0.055, 0.408, 0.596)]# Lichtgroen naar middelgroen
+colors = [(0.055, 0.408, 0.596), (0.773, 0.890, 0.933)]
 cmap = LinearSegmentedColormap.from_list('custom_cmap', colors)
 
 
 # Voorbeeldgegevens van een 5x6 tabel
-r2 = [[0.22, 0.24, 0.23],
-       [0.08, 0.22, 0.21],
-       [0.06, 0.08, 0.14],
-        [0.19, 0.17, 0.19],
-        [0.20, 0.27, 0.22]]
+#r2 = [[0.22, 0.24, 0.23],
+ #      [0.08, 0.22, 0.21],
+  #     [0.06, 0.08, 0.14],
+   #     [0.19, 0.17, 0.19],
+    #    [0.20, 0.27, 0.22]]
 
-#rmse = [[0.99, 1.05, 1.06],
- #       [0.90,1.26,1.27],
-  #      [1.16,1.20,1.16],
-   #     [1.05,1.04,1.03],
-    #    [1.01,1.08,1.12]]
+rmse = [[0.99, 1.05, 1.06],
+        [0.90,1.26,1.27],
+        [1.16,1.20,1.16],
+        [1.05,1.04,1.03],
+        [1.01,1.08,1.12]]
 
-data=np.array(r2)
+data=np.array(rmse)
 
 y_labels = ['MM1s', 'MOLP2','EJM', 'KMS34', 'KSM18']
 x_labels = ['RF', 'SVR', 'XGBoost']
@@ -50,9 +50,11 @@ for i in range(data.shape[0]):
     for j in range(data.shape[1]):
         ax.text(j, i, f'{data[i, j]:.2f}', ha='center', va='center', color='black')
 
-# Voeg een titel toe aan de plot
-ax.set_title('R2 Values\nCCLs in computational models\n ')
-ax.set_ylabel('CCLs\n ')
+# Titel
+ax.set_title('RMSE Values\n Testing Cell Lines & Computational Models\n',fontweight='bold')
+
+
+ax.set_ylabel('MM Cell Lines\n ')
 #ax.set_xlabel('Computational Models')
 
 # Toon de plot
